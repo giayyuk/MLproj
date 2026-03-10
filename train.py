adabb41059b721fe2e16aa4cd4fb06822b4ea731
giayyuk <is.gi.yue@gmail.com>
2026-03-10 14:53:39 +0000

    empty commit lul

--- train.py
+++ train.py
@@ -1,9 +1,9 @@
 import torch
-import tdqm
+import tqdm
 import random
-import sn
-import numpy as np4
-import src.data.load_data
+from sn import FoodSN
+import numpy as np
+from src.data.load_data import Data
 from torch.utils.data import DataLoader,random_split
 from torch.nn import CrossEntropyLoss
 
@@ -11,13 +11,18 @@ torch.manual_seed(42)
 random.seed(42)
 np.random.seed(42)
 torch.cuda.manual_seed_all(42)
-torch.cuda.memory.set_per_process_memory_fraction(fraction=0.33)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+torch.cuda.memory.set_per_process_memory_fraction(fraction=0.33)
+torch.set_num_threads(3)
 
 # loss optimizer model
 loss_fn = CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters())
 model = FoodSN()
+optimizer = torch.optim.Adam(model.parameters())
+
+# load data
+data = Data()
+train_data = data.get_train_data()
 
 
 def train(num_epochs, model,data_loader):
@@ -31,4 +36,6 @@ def train(num_epochs, model,data_loader):
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+    torch.save(model.state_dict(), "food101_first_training.pth")
 
+train(2, model, train_data)
