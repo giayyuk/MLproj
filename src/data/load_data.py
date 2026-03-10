aaa87e29e10846561f14527d5b116ded97c1e1d6
SimoneU16 <simone.unruh@ruhr-uni-bochum.de>
2026-03-10 14:27:20 +0100

    initialized some data

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,7 +1,15 @@
 import torchvision
 
-train_split = torchvision.datasets.Food101('src/data/train',split="train",download=True)
-test_split = torchvision.datasets.Food101('src/data/test',split="test",download=True)
+class Data():
+    def __init__(self):
+        self.train_data = torchvision.datasets.Food101('src/data/train',split="train",download=True)
+        self.test_data = torchvision.datasets.Food101('src/data/test',split="test",download=True)
 
-if __name__ == "__main__":
-    print("dataSet loaded") if train_split != None and test_split != None else print("failed loading dataSet")
+        if __name__ == "__main__":
+            print("dataSet loaded") if train_split != None and test_split != None else print("failed loading dataSet")
+
+    def get_train_data(self):
+        return self.train_data
+
+    def get_test_data(self):
+        return self.test_data
--- train.py
+++ train.py
@@ -1,8 +1,17 @@
-import sn.py
-
+import sn
+from src.data.load_data import Data
 
 num_classes_for_Food101: int = 101
-data = train_split
-model = FoodSN()
+data = Data()
+train_data = data.get_train_data()
+#test_data = data.get_test_data()
 
-model.fit(data)
+print(type(train_data[0]))
+print(len(train_data))
+#print(test_data)
+
+
+#model = FoodSN()
+
+#model.fit(data)
+
